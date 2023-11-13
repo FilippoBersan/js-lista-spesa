c68cae2bb88b5e5bce6490ea691fba930ace85fb
FilippoBersan <filippobersan99@gmail.com>
2023-11-13 18:22:36 +0100

    feat: lista con while

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const lista = document.querySelector('.list');
+
 const spesa = [
   'Pane',
   'Insalata',
@@ -11,4 +13,15 @@ const spesa = [
   'Kiwi',
   'Vino',
 ];
-console.log(spesa);
+
+let i = 0;
+let text = '';
+while (spesa[i]) {
+  text += spesa[i];
+  i++;
+
+  const prodotto = document.createElement('li');
+  prodotto.classList.add('prodotto');
+  prodotto.textContent = spesa[i];
+  lista.append(prodotto);
+}
